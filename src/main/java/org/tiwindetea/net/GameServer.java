3e7dc67c2c8bfc83db0e0e90d571e7b3fe4c6e4b
Lucas Lazare <lazarelucas@yahoo.fr>
2016-11-20 15:26:17 +0100

    Starting the implementation of GameServer

--- src/main/java/org/tiwindetea/net/GameServer.java
+++ src/main/java/org/tiwindetea/net/GameServer.java
@@ -24,10 +24,135 @@
 
 package org.tiwindetea.net;
 
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryonet.Connection;
+import com.esotericsoftware.kryonet.Server;
+
+import java.io.IOException;
+
+/**
+ * The game server class
+ * This class is used as a host for the logic game
+ * and re dispatches what happened to its clients
+ *
+ * @author Lucas Lazare
+ * @see GameClient
+ * @since 0.1.0
+ */
 public class GameServer {
 
-	public void start() {
+	//Scheduler scheduler;
+	private final Server server = new Server();
 
+	private final Room room = new Room();
+	private final Listener listener = new Listener();
+	private final UDPListener udpListener = new UDPListener();
+	private boolean isRunning = false;
+
+	public GameServer() {
+		this(null, null);
 	}
 
+	public GameServer(String gameName) {
+		this(gameName, null);
+	}
+
+	public GameServer(String gameName, String gamePassword) {
+		this.room.setGameName(gameName);
+		this.room.setGamePassword(gamePassword);
+		this.udpListener.setRoom(this.room);
+		Kryo kryo = this.server.getKryo();
+		//kryo.register(); //Todo
+	}
+
+	/**
+	 * Sets the game password
+	 *
+	 * @param gamePassword new password for the game
+	 * @throws IllegalStateException if the server is already running
+	 */
+	public void setGamePassword(String gamePassword) {
+		if (this.isRunning) {
+			throw new IllegalStateException();
+		} else {
+			this.room.setGamePassword(gamePassword);
+		}
+	}
+
+	/**
+	 * Sets the game name
+	 *
+	 * @param gameName new name for the game
+	 * @throws IllegalStateException if the server is already running
+	 */
+	public void setName(String gameName) {
+		if (this.isRunning) {
+			throw new IllegalStateException();
+		} else {
+			this.room.setGameName(gameName);
+		}
+	}
+
+	/**
+	 * Start the server with a given TCP port for client's
+	 * connections and UDP port for client's broacast.
+	 * <p>
+	 * Any incoming requests will be ignored until the {@link GameServer#start()}
+	 * method is called. Clients will still be able to connect, though
+	 *
+	 * @param TCPport Port to use for clients connection on TCP
+	 * @param UDPport Port to use for clients broadcasting when discovering
+	 * @throws IOException If the server could not be opened
+	 */
+	public void bind(int TCPport, int UDPport) throws IOException {
+		this.server.bind(TCPport);
+		this.udpListener.bind(UDPport);
+	}
+
+	/**
+	 * Starts the server if it is not started yet,
+	 * closes all connections that have been previously done
+	 * and start treating incoming connections and requests.
+	 * <p>
+	 * Basically, starts the game
+	 */
+	public void start() {
+		this.isRunning = true;
+		for (Connection connection : this.server.getConnections()) {
+			connection.close();
+		}
+		if (!this.udpListener.isRunning()) {
+			this.server.start();
+			this.udpListener.start();
+		}
+		this.server.addListener(this.listener);
+	}
+
+	/**
+	 * Closes the TCP server together with the
+	 * UDP server.
+	 */
+	public void stop() {
+		this.isRunning = false;
+		if (this.udpListener.isRunning()) {
+			this.udpListener.stop();
+		}
+		this.server.stop();
+		this.server.removeListener(this.listener);
+	}
+
+	/**
+	 * returns true if the server is running
+	 */
+	public boolean isRunning() {
+		return this.isRunning;
+	}
+
+	private class Listener extends com.esotericsoftware.kryonet.Listener.ReflectionListener {
+		//TODO
+		/*
+		public void received(Connection connection, String string){
+		}
+		 */
+	}
 }
