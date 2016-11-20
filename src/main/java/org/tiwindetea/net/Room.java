e267a40e894836ff63a132111b4c38fe45338d2c
Lucas Lazare <lazarelucas@yahoo.fr>
2016-11-20 15:27:35 +0100

    Implemeting Room class

--- src/main/java/org/tiwindetea/net/Room.java
+++ src/main/java/org/tiwindetea/net/Room.java
@@ -25,14 +25,83 @@
 package org.tiwindetea.net;
 
 import java.io.Serializable;
+import java.util.Collections;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Room implements Serializable {
 
     private String gameName;
 
+    private transient String gamePassword;
+
     private List<String> members;
 
-    private String password;
+    private boolean isLocked;
 
+    Room() {
+        this.gameName = null;
+        this.members = new LinkedList<>();
+        this.gamePassword = null;
+        this.isLocked = false;
+    }
+
+    Room(String gameName) {
+        this.gameName = gameName;
+        this.members = new LinkedList<>();
+        this.gamePassword = null;
+        this.isLocked = false;
+    }
+
+    Room(String gameName, String gamePassword) {
+        this.gameName = gameName;
+        this.members = new LinkedList<>();
+        this.gamePassword = gamePassword;
+        this.isLocked = (gamePassword != null);
+    }
+
+    Room(String gameName, String gamePassword, List<String> members) {
+        this.gameName = gameName;
+        this.members = members;
+        this.gamePassword = gamePassword;
+        this.isLocked = (gamePassword != null);
+    }
+
+    public boolean isLocked() {
+        return this.isLocked;
+    }
+
+    public String getGameName() {
+        return this.gameName;
+    }
+
+    public List<String> getMembers() {
+        return Collections.unmodifiableList(this.members);
+    }
+
+    public boolean checkPassword(String password) {
+        return password == this.gamePassword || (password != null && password.equals(this.gamePassword));
+    }
+
+    void addMember(String memberName) {
+        this.members.add(memberName);
+    }
+
+    void removeMember(String memberName) {
+        this.members.remove(memberName);
+    }
+
+    void setGameName(String gameName) {
+        this.gameName = gameName;
+    }
+
+    void setGamePassword(String gamePassword) {
+        this.gamePassword = gamePassword;
+        this.isLocked = gamePassword != null;
+    }
+
+
+    public String toString() {
+        return this.gameName;
+    }
 }
