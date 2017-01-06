db9b3fd7b71a655bfa95ce06bd6ec67026a4e213
Jérôme BOULMIER <jerome.boulmier@outlook.fr>
2017-01-06 17:55:26 +0100

    Adding getPlayer in PlayerCapacity.

--- src/main/java/org/tiwindetea/animewarfare/logic/capacity/PlayerCapacity.java
+++ src/main/java/org/tiwindetea/animewarfare/logic/capacity/PlayerCapacity.java
@@ -2,10 +2,23 @@ package org.tiwindetea.animewarfare.logic.capacity;
 
 import org.tiwindetea.animewarfare.logic.Player;
 
+import java.lang.ref.WeakReference;
+import java.util.Objects;
+
 abstract class PlayerCapacity implements Capacity {
-	protected final Player player;
+	private final WeakReference<Player> player;
 
 	protected PlayerCapacity(Player player) {
-		this.player = player;
+		this.player = new WeakReference<>(player);
 	}
+
+	public Player getPlayer() {
+		Player player = this.player.get();
+
+		Objects.requireNonNull(player);
+
+		return player;
+	}
+
+	//protected abstract void destroy();
 }
