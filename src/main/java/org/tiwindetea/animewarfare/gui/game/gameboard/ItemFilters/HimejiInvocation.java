13380177543760e200a7b14828e699cda9304488
Jérôme BOULMIER <jerome.boulmier@outlook.fr>
2017-01-13 10:04:55 +0100

    Fixup 5def19d353ff04c2b0c6add4debebc5863f92d3d

--- src/main/java/org/tiwindetea/animewarfare/gui/game/gameboard/ItemFilters/HimejiInvocation.java
+++ src/main/java/org/tiwindetea/animewarfare/gui/game/gameboard/ItemFilters/HimejiInvocation.java
@@ -13,15 +13,14 @@ import org.tiwindetea.animewarfare.logic.units.UnitLevel;
 import org.tiwindetea.animewarfare.logic.units.UnitType;
 import org.tiwindetea.animewarfare.net.networkrequests.client.NetInvokeUnitRequest;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class HimejiInvocation extends AbstractZoneFilter {
 	@Override
 	public List<MenuItem> apply(FactionType factionType, Integer zone) {
-		if (GamePhaseMonitor.getCurrentPhase() == PhaseChangedEvent.Phase.ACTION
-				|| GlobalChat.getClientFaction(PlayerTurnMonitor.getCurrentPlayer()) == factionType
+		if (GamePhaseMonitor.getCurrentPhase() != PhaseChangedEvent.Phase.ACTION
+				|| GlobalChat.getClientFaction(PlayerTurnMonitor.getCurrentPlayer()) != factionType
 				|| GameLayoutController.getMap().getStudiosOf(UnitType.HIMEJI_MIZUKI.getDefaultFaction()).isEmpty()) {
 			return Collections.emptyList();
 		}
@@ -29,16 +28,16 @@ public class HimejiInvocation extends AbstractZoneFilter {
 		if (checkHimejiInvocationConditions(factionType, zone)) {
 			int cost = UnitType.HIMEJI_MIZUKI.getDefaultCost() + CostModifierMonitor.getUnitCostModifier(UnitType.HIMEJI_MIZUKI);
 
+			MenuItem menuItem = new MenuItem("Invoke Himeji (" + cost + " SP)");
+			menuItem.setOnAction(e -> MainApp.getGameClient()
+			                                 .send(new NetInvokeUnitRequest(UnitType.HIMEJI_MIZUKI,
+					                                 zone)));
 			if (GameLayoutController.getLocalPlayerInfoPane()
 			                        .getStaffCounter()
-			                        .getValue() >= cost) {
-				MenuItem menuItem = new MenuItem("Invoke Himeji");
-				menuItem.setOnAction(e -> MainApp.getGameClient()
-				                                 .send(new NetInvokeUnitRequest(UnitType.HIMEJI_MIZUKI,
-						                                 zone)));
-
-				return Arrays.asList(menuItem);
+			                        .getValue() < cost) {
+				menuItem.setDisable(true);
 			}
+			return Collections.singletonList(menuItem);
 		}
 
 		return Collections.emptyList();
--- src/main/java/org/tiwindetea/animewarfare/gui/game/gameboard/ItemFilters/LelouchInvocation.java
+++ src/main/java/org/tiwindetea/animewarfare/gui/game/gameboard/ItemFilters/LelouchInvocation.java
@@ -7,33 +7,34 @@ import org.tiwindetea.animewarfare.gui.game.CostModifierMonitor;
 import org.tiwindetea.animewarfare.gui.game.GameLayoutController;
 import org.tiwindetea.animewarfare.gui.game.GamePhaseMonitor;
 import org.tiwindetea.animewarfare.gui.game.PlayerTurnMonitor;
+import org.tiwindetea.animewarfare.gui.game.UnitCountMonitor;
 import org.tiwindetea.animewarfare.gui.game.gameboard.GUnit;
 import org.tiwindetea.animewarfare.logic.FactionType;
 import org.tiwindetea.animewarfare.logic.states.events.PhaseChangedEvent;
 import org.tiwindetea.animewarfare.logic.units.UnitType;
 import org.tiwindetea.animewarfare.net.networkrequests.client.NetInvokeUnitRequest;
 
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 public class LelouchInvocation extends AbstractUnitFilter {
 	@Override
 	public List<MenuItem> apply(FactionType factionType, GUnit gUnit) {
-		if (GamePhaseMonitor.getCurrentPhase() == PhaseChangedEvent.Phase.ACTION && GlobalChat.getClientFaction(
-				PlayerTurnMonitor.getCurrentPlayer()) == factionType) {
-			if (gUnit.getType() == UnitType.CC) {
+		if (GamePhaseMonitor.getCurrentPhase() == PhaseChangedEvent.Phase.ACTION
+				&& GlobalChat.getClientFaction(PlayerTurnMonitor.getCurrentPlayer()) == factionType) {
+			if (!UnitCountMonitor.getInstance().hasBeenInvoked(UnitType.LELOUCH) && gUnit.getType() == UnitType.CC) {
 				int cost = UnitType.LELOUCH.getDefaultCost() + CostModifierMonitor.getUnitCostModifier(UnitType.LELOUCH);
+				MenuItem menuItem = new MenuItem("Replace by Lelouch (" + cost + " SP).");
+				menuItem.setOnAction(e -> MainApp.getGameClient()
+				                                 .send(new NetInvokeUnitRequest(UnitType.LELOUCH,
+						                                 gUnit.getZone())));
 				if (GameLayoutController.getLocalPlayerInfoPane()
 				                        .getStaffCounter()
-				                        .getValue() >= cost) {
-					MenuItem menuItem = new MenuItem("Replace by Lelouch");
-					menuItem.setOnAction(e -> MainApp.getGameClient()
-					                                 .send(new NetInvokeUnitRequest(UnitType.LELOUCH,
-							                                 gUnit.getZone())));
-
-					return Arrays.asList(menuItem);
+				                        .getValue() < cost) {
+					menuItem.setDisable(true);
 				}
+
+				return Collections.singletonList(menuItem);
 			}
 		}
 
